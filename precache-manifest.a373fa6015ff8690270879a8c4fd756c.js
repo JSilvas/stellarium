2ab4d6aa0073a1675f91a4b87fd4bc04a73fc8d3
Jay Silvas <jaywsilvas@gmail.com>
2019-10-30 18:51:30 -0700

    Updates

---
+++ precache-manifest.a373fa6015ff8690270879a8c4fd756c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09e80f001ecfc878b2de9426ebb0d45e",
+    "url": "/Stellarium/index.html"
+  },
+  {
+    "revision": "8e1e1897a6e9f504be73",
+    "url": "/Stellarium/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "88d32971a4e2a25a466b",
+    "url": "/Stellarium/static/js/2.b49ab61c.chunk.js"
+  },
+  {
+    "revision": "8e1e1897a6e9f504be73",
+    "url": "/Stellarium/static/js/main.745fcc73.chunk.js"
+  },
+  {
+    "revision": "39ef03876d16a6b13869",
+    "url": "/Stellarium/static/js/runtime-main.bc09ad0d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Stellarium/static/media/logo.25bf045c.svg"
+  }
+]);
